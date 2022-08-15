3096fec22c3bf29f5d0944d7b23f4f525a47e31c
Wondo-K <wondok95@naver.com>
2022-08-15 15:28:20 +0900

    시큐리티이전백업

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,5 +1,7 @@
 package com.cos.blog.controller.api;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +21,9 @@ public class UserApiController {
 	@Autowired
 	private UserService userService;
 	
+	@Autowired
+	private HttpSession session;
+	
 	@PostMapping("/api/user")
 	public ResponseDto<Integer> save(@RequestBody User user) { // user => username, password, email
 		System.out.println("UserApiController : save 호출됨");
@@ -26,5 +31,14 @@ public class UserApiController {
 		user.setRole(RoleType.USER); //user에 role이 없기 때문에 설정해주어야 한다
 		userService.회원가입(user);
 		return new ResponseDto<Integer>(HttpStatus.OK.value(),1); //자바 오브젝트를 JSON으로 변환해서 리턴(Jackson)
+	}
+	@PostMapping("/api/user/login")
+	public ResponseDto<Integer> login(@RequestBody User user){
+		System.out.println("UserApiController : login 호출됨");
+		User principal = userService.로그인(user); //principal 접근주체
+		if(principal!=null) {
+			session.setAttribute("principal", principal);		
+		}
+		return new ResponseDto<Integer>(HttpStatus.OK.value(),1); 
 	}
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.cos.blog.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.cos.blog.model.User;
 
@@ -10,5 +11,13 @@ import com.cos.blog.model.User;
 public interface UserRepository extends JpaRepository<User, Integer>{ //JpaRepository는 user가 관리하는 Repository이고 user테이블의 기본키는 integer(정수,숫자)라는 뜻
 																	  // JpaRepository는 findAll()을 가지고 있는데 user테이블이 가지고 있는 모든 행을 리턴한다
 																	  // 기본적인 crud 관련 함수를 들고 있음
+	//JPA쿼리
+	// SELECT * FROM user WHERE username=1? AND password=2?; 
+	User findByUsernameAndPassword(String username, String password);
 
+	/*
+	 * @Query(
+	 * value="SELECT * FROM user WHERE username=?1 AND password=?2, nativeQuery=true"
+	 * ) User login(String username, String password);
+	 */
 }
